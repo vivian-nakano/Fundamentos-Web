87e6ae895909139ff1884876bb35a004009edf47
unknown <vrnakano@gmail.com>
2020-11-24 14:24:24 -0300

    Aplicação do JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+/* isto é um escopo!
+
+JS: case sensitive, reconhece as letras minu e maiusculas.
+
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementsByName()
+por Classe: getElementsByClassName() --> vários elementos por ser classe...
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email') /* ('input#email) se email fosse classe seria ponto .*/
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+nome.style.width='100%'
+email.style.width='100%'
+
+function validaNome() {
+
+        let txtNome = document.querySelector('#txtNome')
+
+        if (nome.value.length < 3) {
+            txtNome.innerHTML = 'Nome Inválido'
+            txtNome.style.color = 'red'
+        } else {
+            txtNome.innerHTML = 'Nome Válido'
+            txtNome.style.color = 'green'
+            nomeOk = true
+        }
+    }
+
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML='Email inválido'
+        txtEmail.style.color='red'
+    } else {
+        txtEmail.innerHTML='Email válido'
+        txtEmail.style.color='green'
+        emailOk = true
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >= 200) {
+            txtAssunto.innerHTML = 'Assunto do texto é muito grande, insira no máximo 200 caracteres.'
+            txtAssunto.style.color = 'red'
+            txtAssunto.style.display = 'block'
+        } else {
+            txtAssunto.style.display = 'none'
+            emailOk = true
+        }
+    }
+
+    function enviar() {
+        if (nomeOk == true && emailOk == true && emailOk == true) {
+            alert('Formulário enviado com sucesso!')
+        } else {
+            alert ('Preencha o formulário corretamente antes de enviar...')
+        }
+    
+    }
+
+    function mapaZoom() {
+        mapa.style.width = '700px'
+        mapa.style.height = '500px'
+     }
+     
+     function mapaNormal() {
+        mapa.style.width = '400px'
+        mapa.style.height = '250px'
+     }
